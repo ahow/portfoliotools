a3222c6971770dfa5d5d1e3992c88f4e06730119
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2016-12-21 20:08:32 +0800

    Fix. User menu cleaned

--- modules/authMenu/user_menu.js
+++ modules/authMenu/user_menu.js
@@ -1,6 +1,4 @@
 [
 {"a":"/profile","t":"MY_PROFILE"},
-{"a":"/admin","t":"Admin panel","acl":["admin"]},
-{"a":"/tables","t":"Database editor","acl":["editor"]},
-{"a":"/confer","t":"Configurator","acl":["editor"]}
+{"a":"/admin","t":"Admin panel","acl":["admin"]}
 ]
