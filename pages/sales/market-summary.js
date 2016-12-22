8407419bd950506797b7980f895dc3f9e0ea1615
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2016-12-22 13:21:09 +0800

    Optimization Market summary

--- pages/sales/market-summary.js
+++ pages/sales/market-summary.js
@@ -2,7 +2,7 @@ $(function(){
     
     function toFloat(v, decimals)
     { var n = 1.0*v;            
-      if (isNaN(n) || v==null || (''+v).toLowerCase()=='null')
+      if (isNaN(n) || v==null)
       {   return '-';
       }
       return n.toFixed(decimals);
