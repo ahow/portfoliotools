93c84dd50c8409566dffec99886b03ef1bce9d52
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2016-12-23 19:56:14 +0800

    Search by Subsector and by Industry group in Companies

---
+++ pages/sales/models/model.company_subsector.js
@@ -0,0 +1,9 @@
+{ "table": "sales_companies",
+  "select": "select distinct subsector as id, subsector as name from $table $where $order",
+  "list_columns": "name",
+  "primary_key": "id",
+  "default_order":"subsector",
+  "search":"subsector like :search",
+  "select_row": "select * from $table where id=:id",
+  "select_total": "select count(*) from $table $where"
+}
