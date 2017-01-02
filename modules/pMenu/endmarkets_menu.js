a68bb8e4d8d72a04739dc95bac8eec501619b990
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-01-02 19:39:17 +0800

    Industry analisys menu

--- modules/pMenu/endmarkets_menu.js
+++ modules/pMenu/endmarkets_menu.js
@@ -2,5 +2,6 @@
 {"a":"/sales/industry-groups","t":"INDUSTRY_GROUPS"},
 {"a":"/sales/sic","t":"SIC"},
 {"a":"/sales/companies","t":"companies"},
-{"a":"/sales/market-summary","t":"market-summary"}
+{"a":"/sales/market-summary","t":"market-summary"},
+{"a":"/sales/industry-analysis","t":"industry-analysis"}
 ]
