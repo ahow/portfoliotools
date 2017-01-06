c2855452683265b737416f15bafe4f024ebcf41f
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-01-06 16:42:35 +0800

    Move Industry analysis to Companies analysis

---
+++ pages/sales/companies-analysis.php
@@ -0,0 +1,30 @@
+<ol class="breadcrumb">
+  <li><a href="<?=mkURL('/sales')?>"><?=T('Sales')?></a></li>
+  <li class="active" ><?=$this->cfg->title?></li>
+</ol>
+<?php
+    include(__DIR__.'/bsforms.php');
+    $f = new BSformDefault();
+?>
+<div id="mranking">
+    <div class="row">
+        <div class="col-lg-6"><?=$f->search3dot('sic_code','sic')?></div>
+        <div class="col-lg-4"><?=$f->modelSelect('region','/pages/sales/Model/regions')?></div>
+        <div class="col-lg-2"><?=$f->input('minsize','number')?></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6"><?=$f->search3dot('subsec','subsector')?></div>
+        <div class="col-lg-4"></div>
+        <div class="col-lg-2"><button style="margin-top:25px" class="btn btn-primary b-vchart">View Chart</button></div>
+    </div>
+    <div class="row">
+        <?php $list = 'Total sales;% top 3;% top 5;Stability;Sales growth;ROIC;PE;EVBIDTA;Payout;% reviewed'; ?>
+        <div class="col-lg-6"><?=$f->listSelect('x-axis',$list)?></div>
+        <div class="col-lg-6"><?=$f->listSelect('y-axis',$list)?></div>
+    </div>
+    <div class="row">
+        <div id="container" style="min-width: 210px; height: 400px; margin: 0 auto"></div>
+    </div>
+</div>
+<div id="search_sic"></div>
+<div id="search_subsec"></div>
