559eafa24dfe4fdbe402cb28960db9269f2425b0
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-01-24 08:40:34 +0800

    Print button in Theme exposures

--- pages/sales/theme-exposures.php
+++ pages/sales/theme-exposures.php
@@ -11,7 +11,12 @@
     <div class="row">
         <div class="col-lg-5"><?=$f->modelSelect('portfolio','/pages/sales/Model/portfolio-lookup')?></div>
         <div class="col-lg-5"><?=$f->modelSelect('comparison','/pages/sales/Model/portfolio-lookup')?></div>
-        <div class="col-lg-2"><button style="margin-top:25px" class="btn btn-primary b-vchart">View Chart</button></div>
+        <div class="col-lg-2">
+            <div class="btn-group">
+                <button style="margin-top:25px" class="btn btn-primary b-print" disabled="true">Print</button>
+                <button style="margin-top:25px" class="btn btn-primary b-vchart">View Chart</button>
+            </div>
+        </div>
     </div>
     <div class="row">
         <div class="col-lg-12"><?=$f->textarea('description')?></div>
