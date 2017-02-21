6f0d5a74726387049ea824c4537ec07810f0ca7f
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-02-21 16:26:07 +0800

    Portfolio summaries added

--- modules/pMenu/pfmeasures_menu.js
+++ modules/pMenu/pfmeasures_menu.js
@@ -1,4 +1,5 @@
 [
 {"a":"/sales/theme-exposures","t":"theme-exposures"},
-{"a":"/sales/portfolio-metrics","t":"portfolio-metrics"}
+{"a":"/sales/portfolio-metrics","t":"portfolio-metrics"},
+{"a":"/sales/portfolio-summaries","t":"portfolio-summaries"}
 ]
