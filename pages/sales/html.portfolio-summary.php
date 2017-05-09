4f13560b8748314dd1944dee77f33b5460c5d2da
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-05-09 19:43:12 +0800

    Create portfolio summary page

---
+++ pages/sales/html.portfolio-summary.php
@@ -0,0 +1,7 @@
+<?php 
+include('empty_page.php');
+emptyHeader('Portfolio summary'); ?>
+<div class="container">
+<h1>Portfolio summary</h1>
+</div> <!-- /container -->
+<?php emptyFooter(array('portfolio-snapshot.js', 'portfolio-snapshot-html.js')); ?>
