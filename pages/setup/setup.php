6fe749943b3aab82766da63395120a992cbd2780
Vitaliy Fedotov <kursruk@yandex.ru>
2017-06-06 03:10:47 +0800

    Fix deprecated constructor name

--- pages/setup/setup.php
+++ pages/setup/setup.php
@@ -1,6 +1,6 @@
 <?php
   class setup extends wPage
-  {  function setup($cfg, $path, $seg=null)
+  {  function __construct($cfg, $path, $seg=null)
      {  $cfg->title = 'DATABASE_SETUP';
         $this->path = $path.'/index.php';
         $this->cfg = $cfg;
