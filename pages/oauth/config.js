e18569d13503e04d3439802aeec2d9058abaf105
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-09-18 19:33:25 +0800

    add Google oauth

--- pages/oauth/config.js
+++ pages/oauth/config.js
@@ -2,5 +2,5 @@
   {"name":"twitter","title":"Twitter","enabled":false,"key":"your_key","secret":"you_secret"},
   {"name":"vkontakte","title":"VKontakte","enabled":false,"key":"your_key","secret":"you_secret"},
   {"name":"mailru","title":"VKontakte","enabled":false,"key":"your_key","secret":"you_secret"},
-  {"name":"google","title":"Google","enabled":false,"key":"472482783957-8r967e1d73lrebj6ib23o2unlkkbig38.apps.googleusercontent.com","secret":"VWezp8cn6s5MMrocmmKfLsTM"}
+  {"name":"google","title":"Google","enabled":true,"key":"472482783957-8r967e1d73lrebj6ib23o2unlkkbig38.apps.googleusercontent.com","secret":"VWezp8cn6s5MMrocmmKfLsTM"}
 ]
