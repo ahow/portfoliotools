c3ac58a78a9df2772a95db33fd6e42f85443eb3d
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-10-15 17:33:02 +0800

    Fix of Division Details.csv downloading

--- pages/sales/html.datadetails.csv.php
+++ pages/sales/html.datadetails.csv.php
@@ -18,10 +18,13 @@ if ($this->inGroup('admin') || $this->inGroup('editor'))
       { $h[] = "me $i";
         $h[] = "SIC $i";
         $h[] = "Sales $i";
+        $h[] = "EBIT $i";
+        $h[] = "Assets $i";
+        $h[] = "Capex $i";
       }
       
       
-      $qr=$db->query('select cid,division,syear,me,sic,sales from sales_divdetails order by 1,2,3 desc');
+      $qr=$db->query('select cid,division,syear,me,sic,sales,ebit,assets,capex from sales_divdetails order by 1,2,3 desc');
       
 
       $fp = fopen('php://output', 'w');
@@ -39,13 +42,19 @@ if ($this->inGroup('admin') || $this->inGroup('editor'))
                {  $row[$n] = $pr[$i][0];
                   $row[$n+1] = $pr[$i][1];
                   $row[$n+2] = $pr[$i][2];
+                  $row[$n+3] = $pr[$i][3];
+                  $row[$n+4] = $pr[$i][4];
+                  $row[$n+5] = $pr[$i][5];
                   
                } else
                {  $row[$n] = '';
                   $row[$n+1] = '';
                   $row[$n+2] = '';
+                  $row[$n+3] = '';
+                  $row[$n+4] = '';
+                  $row[$n+5] = '';
                }
-               $n+=3;
+               $n+=6;
              }
              fputcsv($fp, $row, $delim); 
              $last = $r->division;
@@ -58,6 +67,9 @@ if ($this->inGroup('admin') || $this->inGroup('editor'))
           $pr[$r->syear][0] = $r->me;
           $pr[$r->syear][1] = $r->sic;
           $pr[$r->syear][2] = $r->sales;                    
+          $pr[$r->syear][3] = $r->ebit;                    
+          $pr[$r->syear][4] = $r->assets;                    
+          $pr[$r->syear][5] = $r->capex;                    
       }
 
      // Last row
@@ -67,13 +79,19 @@ if ($this->inGroup('admin') || $this->inGroup('editor'))
        {  $row[$n] = $pr[$i][0];
           $row[$n+1] = $pr[$i][1];
           $row[$n+2] = $pr[$i][2];
+          $row[$n+3] = $pr[$i][3];
+          $row[$n+4] = $pr[$i][4];
+          $row[$n+5] = $pr[$i][5];
           
        } else
        {  $row[$n] = '';
           $row[$n+1] = '';
           $row[$n+2] = '';
+          $row[$n+3] = '';
+          $row[$n+4] = '';
+          $row[$n+5] = '';
        }
-       $n+=3;
+       $n+=6;
      }
      fputcsv($fp, $row,  $delim); 
       
