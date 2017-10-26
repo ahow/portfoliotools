62b88e82f1eb5b78e63e83ef3af7476c319d38d5
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-10-26 20:07:58 +0800

    Refactoring

--- ajx-framework/pages/sales/sales.php
+++ ajx-framework/pages/sales/sales.php
@@ -19,108 +19,111 @@
         } else $this->cpage=__DIR__."/index.php";
        $this->cfg->addJs('/js', 'formvalidator.js');
        $this->cfg->addJs('/js', 'models.js');
-       if ($this->nav=='upload')
-       {   $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
-           $this->cfg->addJs('/html.php/pages/sales','upload.js');
+       switch ($this->nav)
+       {
+           case 'upload':
+                $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
+                $this->cfg->addJs('/html.php/pages/sales','upload.js');    
+           break;           
+           case 'sic':
+                $this->cfg->addJs('/html.php/pages/sales','download.js');
+                $this->cfg->addJs('/html.php/pages/sales','sic.js');
+           break;           
+           case 'companies':
+               $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+               $this->cfg->addJs('/html.php/pages/sales','companies.js');
+               $this->cfg->addJs('/bootstrap-3.3.6','bootstrap3-typeahead.min.js');
+           break;
+           case 'market-ranking':
+               $this->cfg->addJs('/html.php/pages/sales','market-ranking.js'); 
+           break;
+           case 'market-summary':
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+               $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+               $this->cfg->addJs('/html.php/pages/sales','market-summary.js'); 
+           break;                      
+           case 'themes-summary':
+               $this->cfg->addJs('/js','bootstrap-slider.js');
+               $this->cfg->addJs('/js', 'loadingoverlay.min.js');
+               $this->cfg->addCSS('/css','bootstrap-slider.css');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+               $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+               $this->cfg->addJs('/html.php/pages/sales','themes-summary.js');
+           break;                      
+           case 'portfolio-list':
+               $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
+               $this->cfg->addJs('/html.php/pages/sales','portfolio-list.js'); 
+           break;                      
+           case 'metric-list':
+               $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
+               $this->cfg->addJs('/html.php/pages/sales','metric-list.js');
+           break;                      
+           case 'theme-exposures':
+               $this->cfg->addJs('/html.php/pages/sales','mdselect.js');
+               $this->cfg->addJs('/html.php/pages/sales','theme-exposures.js');
+               $this->cfg->addJs('/js', 'loadingoverlay.min.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+           break;                      
+           case 'portfolio-metrics':
+               $this->cfg->addJs('/html.php/pages/sales','portfolio-metrics.js');
+               $this->cfg->addJs('/html.php/pages/sales','stacked.js');
+               $this->cfg->addJs('/html.php/pages/sales','stacked-gradient.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');  
+           break;                      
+           case 'companies-analysis':
+                $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+                $this->cfg->addJs('/html.php/pages/sales','download.js');
+                $this->cfg->addJs('/html.php/pages/sales','companies-analysis.js');
+                $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+                $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+                $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js'); 
+           break;
+           case 'industry-analysis':
+                $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+                $this->cfg->addJs('/html.php/pages/sales','download.js');
+                $this->cfg->addJs('/html.php/pages/sales','industry-analysis.js');
+                $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+                $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+                $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+           break;
+           case 'themes-comparison':
+               $this->cfg->addJs('/js','bootstrap-slider.js');
+               $this->cfg->addJs('/js', 'loadingoverlay.min.js');
+               $this->cfg->addCSS('/css','bootstrap-slider.css');
+               $this->cfg->addJs('/html.php/pages/sales','lookup.js');
+               $this->cfg->addJs('/html.php/pages/sales','download.js');
+               $this->cfg->addJs('/html.php/pages/sales','themes-comparison.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+           break;
+           case 'portfolio-summaries':
+               $this->cfg->addJs('/js', 'loadingoverlay.min.js');
+               $this->cfg->addJs('/html.php/pages/sales','stacked.js');
+               $this->cfg->addJs('/html.php/pages/sales','stacked-gradient.js');
+               $this->cfg->addJs('/html.php/pages/sales','circles-chart.js');
+               $this->cfg->addJs('/html.php/pages/sales','circles-best-worst.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
+               $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
+               $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
+               $this->cfg->addJs('/html.php/pages/sales','mdselect.js');
+               $this->cfg->addJs('/html.php/pages/sales','portfolio-summary-charts.js');
+               $this->cfg->addJs('/html.php/pages/sales','portfolio-summaries.js');
+               $this->cfg->addJs('/html.php/pages/sales','portfolio-snapshot.js');
+           break;
+           case 'thematic-industry-comparison':
+           
+           break;
+           default: $this->cfg->addJs('/html.php/pages/sales','inittable.js');               
        }
-       else if ($this->nav=='companies')
-       {   $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','companies.js');
-           $this->cfg->addJs('/bootstrap-3.3.6','bootstrap3-typeahead.min.js');
-       } 
-       else if ($this->nav=='sic')
-       {   $this->cfg->addJs('/html.php/pages/sales','download.js');
-           $this->cfg->addJs('/html.php/pages/sales','sic.js');
-       }
-       else if ($this->nav=='market-ranking')
-       {   
-           $this->cfg->addJs('/html.php/pages/sales','market-ranking.js');            
-       }
-       else if ($this->nav=='market-summary')
-       {   $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-           $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','market-summary.js');
-       }
-       else if ($this->nav=='themes-summary')
-       {   $this->cfg->addJs('/js','bootstrap-slider.js');
-           $this->cfg->addJs('/js', 'loadingoverlay.min.js');
-           $this->cfg->addCSS('/css','bootstrap-slider.css');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-           $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','themes-summary.js');
-       }
-       else if ($this->nav=='portfolio-list')
-       {   $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
-           $this->cfg->addJs('/html.php/pages/sales','portfolio-list.js');
-       }
-       else if ($this->nav=='metric-list')
-       {   $this->cfg->addJs('/html.php/pages/sales','previewCSV.js');
-           $this->cfg->addJs('/html.php/pages/sales','metric-list.js');
-       }
-       else if ($this->nav=='theme-exposures')
-       {   $this->cfg->addJs('/html.php/pages/sales','mdselect.js');
-           $this->cfg->addJs('/html.php/pages/sales','theme-exposures.js');
-           $this->cfg->addJs('/js', 'loadingoverlay.min.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-       }
-       else if ($this->nav=='portfolio-metrics')
-       {   $this->cfg->addJs('/html.php/pages/sales','portfolio-metrics.js');
-           $this->cfg->addJs('/html.php/pages/sales','stacked.js');
-           $this->cfg->addJs('/html.php/pages/sales','stacked-gradient.js');
-           // $this->cfg->addJs('/html.php/pages/sales','sectoralloc.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-       }
-       else if ($this->nav=='companies-analysis')
-       {   $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','download.js');
-           $this->cfg->addJs('/html.php/pages/sales','companies-analysis.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-       }
-       else if ($this->nav=='industry-analysis')
-       {   $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','download.js');
-           $this->cfg->addJs('/html.php/pages/sales','industry-analysis.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-       } 
-       else if ($this->nav=='themes-comparison')
-       {   $this->cfg->addJs('/js','bootstrap-slider.js');
-           $this->cfg->addJs('/js', 'loadingoverlay.min.js');
-           $this->cfg->addCSS('/css','bootstrap-slider.css');
-           $this->cfg->addJs('/html.php/pages/sales','lookup.js');
-           $this->cfg->addJs('/html.php/pages/sales','download.js');
-           $this->cfg->addJs('/html.php/pages/sales','themes-comparison.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-       } 
-       else if ($this->nav=='portfolio-summaries')
-       {   $this->cfg->addJs('/js', 'loadingoverlay.min.js');
-           $this->cfg->addJs('/html.php/pages/sales','stacked.js');
-           $this->cfg->addJs('/html.php/pages/sales','stacked-gradient.js');
-           $this->cfg->addJs('/html.php/pages/sales','circles-chart.js');
-           $this->cfg->addJs('/html.php/pages/sales','circles-best-worst.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts.js');
-           $this->cfg->addJs('https://code.highcharts.com','highcharts-more.js');
-           $this->cfg->addJs('https://code.highcharts.com/modules','exporting.js');
-           $this->cfg->addJs('/html.php/pages/sales','mdselect.js');
-           $this->cfg->addJs('/html.php/pages/sales','portfolio-summary-charts.js');
-           $this->cfg->addJs('/html.php/pages/sales','portfolio-summaries.js');
-           $this->cfg->addJs('/html.php/pages/sales','portfolio-snapshot.js');
-       } 
-       else
-       {   $this->cfg->addJs('/html.php/pages/sales','inittable.js');
-       }
+
      }
      
      function afterInit()
