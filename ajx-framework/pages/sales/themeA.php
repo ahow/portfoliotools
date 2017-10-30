3c627bee75e6719f04441fe90e1e3393e56023e4
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-10-30 22:55:37 +0800

    New page. ThemeB

--- ajx-framework/pages/sales/themeA.php
+++ ajx-framework/pages/sales/themeA.php
@@ -1,4 +1,4 @@
-  <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
+<img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
 
   <div class="row">
 
---
+++ ajx-framework/pages/sales/themeB.php
@@ -0,0 +1,2 @@
+ <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
+ 
