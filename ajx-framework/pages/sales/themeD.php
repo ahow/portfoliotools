49cbb708e06fa8e7796d485c5b9cc03d71b0305d
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-11-06 20:31:46 +0800

    Theme page example content

--- ajx-framework/pages/sales/themeD.php
+++ ajx-framework/pages/sales/themeD.php
@@ -1,1 +1,88 @@
+<img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
+
+  <div class="row">
+
+    <!-- /.col-lg-3 -->
+
+    <div class="col-lg-12">
+
+        <div class="card-body">
+          <h3 class="card-title">Theme D</h3>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
+        </div>
+      </div>
+      <!-- /.card -->
+
+      <div class="card card-outline-secondary my-4">
+        <div class="card-header">
+          Overview
+        </div>
+        <div class="card-body">
+          <div class="row">
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+            </div>
+          </div>
+
+
+
+
+
+
+        </div>
+      </div>
+
+      <div class="card card-outline-secondary my-4">
+        <div class="card-header">
+          Drivers
+        </div>
+        <div class="card-body">
+          <div class="row">
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card card-outline-secondary my-4">
+        <div class="card-header">
+          Implications
+        </div>
+        <div class="card-body">
+          <div class="row">
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card card-outline-secondary my-4">
+        <div class="card-header">
+          Industry exposures
+        </div>
+        <div class="card-body">
+          <div class="row">
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
