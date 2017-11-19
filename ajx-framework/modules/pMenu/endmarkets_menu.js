b5eba633a11d011ca1d7c149d87036563c76914c
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-11-19 18:38:17 +0800

    New menu item

--- ajx-framework/modules/pMenu/endmarkets_menu.js
+++ ajx-framework/modules/pMenu/endmarkets_menu.js
@@ -1,4 +1,5 @@
 [
+{"a":"/sales/subsector","t":"Subsector details"},
 {"a":"/sales/industry-groups","t":"INDUSTRY_GROUPS"},
 {"a":"/sales/sic","t":"INDUSTRY_DETAILS"},
 {"a":"/sales/market-summary","t":"market-summary"},
