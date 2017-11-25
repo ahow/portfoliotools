6777b32bc7911459d9d440aa25fbc6f1fb0280b6
Fedotov Vitaliy Victorovich <kursruk@yandex.ru>
2017-11-25 11:20:13 +0800

    Save button fix

--- ajx-framework/pages/sales/html.newdivision.php
+++ ajx-framework/pages/sales/html.newdivision.php
@@ -58,7 +58,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-default btn-success b-save-division disabled">Save</button>
+        <button type="button" class="btn btn-default btn-success b-save-division">Save</button>
       </div>
     </div>
 
