6739bb438e865fe3f0d8aee6e6710c96c0018760
ahow <andyhoward@me.com>
2017-12-02 00:12:57 +0000

    Update index.php

--- ajx-framework/pages/sales/index.php
+++ ajx-framework/pages/sales/index.php
@@ -1,6 +1,7 @@
 <div class="col-lg-12">
-  <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
 </div>
+  
+  <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
   
   <div class="row">
 
