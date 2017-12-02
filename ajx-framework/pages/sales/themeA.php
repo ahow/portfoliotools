4afe39989aba9c263c9f3379bfba1b7299d672cd
ahow <andyhoward@me.com>
2017-12-02 00:01:33 +0000

    Update themeA.php

--- ajx-framework/pages/sales/themeA.php
+++ ajx-framework/pages/sales/themeA.php
@@ -1,4 +1,6 @@
-<img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
+<div class="col-lg-12">
+  <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
+</div>
 
   <div class="row">
 
