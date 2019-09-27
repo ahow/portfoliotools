a030d88b1407148c1e04145360b704364db69048
Fedotov Vitaliy <kursruk@yandex.ru>
2019-09-27 21:48:06 +0800

    Screener companies table

--- ajx-framework/pages/sales/screener.js
+++ ajx-framework/pages/sales/screener.js
@@ -1,3 +1,106 @@
 $(function(){
    $("input.bs-range").slider({});
+
+
+   function modelCompaniesView(selector,d,onclick,ondblclick)
+   {  var s = '';
+      var i;
+      
+      if (d.titles!=undefined)
+      {   var h = '<tr>';
+         for (i in d.titles)
+         { h+='<th>'+d.titles[i]+'</th>';               
+         }
+         h+='</tr>';
+         $(selector).find('thead').html(h);
+      }
+      for (i in d.rows)
+      {   var j;
+         var r = d.rows[i];
+         var cl = '';
+         if (r.reviewed==1) cl=' class="w-reviewed"';
+         if (r.id!=undefined) s+='<tr data-id="'+i+'"'+cl+'>'; else s+='<tr>';
+         for (j in d.columns) 
+         {   if (d.columns[j]=='sales_bn') s+='<td>'+(1.0*r[ d.columns[j] ]).toFixed(2)+'</td>';
+            else s+='<td>'+r[ d.columns[j] ]+'</td>';
+         }
+         s+='</tr>';
+      }
+      $(selector).find('tbody').html(s);
+      if (onclick!=null)  $(selector+' tbody tr').click(function(row){
+         $(row.target).parents('table:first').find('tr').removeClass('active');
+         var id = $(row.target).parents('tr:first').addClass('active').attr('data-id');   
+         onclick(row, d.rows[id]);
+      });
+      
+      if (ondblclick!=undefined && ondblclick!=null)  $(selector+' tbody tr').dblclick(function(row){
+         $(row.target).parents('table:first').find('tr').removeClass('active');
+         var id = $(row.target).parents('tr:first').addClass('active').attr('data-id');   
+         ondblclick(row, d.rows[id]);
+      });
+   }
+
+
+   const model = new modelListController('.model-list', modelCompaniesView);
+   model.morder = 0;
+   
+   model.loaded(function(d){
+       var th = $('#tabsearch .model-list thead th:eq(6)');       
+       th.attr('title','Click to change order');
+       th.html(th.html()+'&nbsp;<span></span>');
+       
+       // Enable an order by sales_bn
+       if (model.morder==1)
+          th.find('span').addClass('glyphicon glyphicon-sort-by-attributes-alt');
+       else if (model.morder==2)
+          th.find('span').addClass('glyphicon glyphicon-sort-by-attributes');
+       
+       th.click(function(e){
+           model.morder++;
+           if (model.morder>2) model.morder=0;
+           if (model.morder==1)
+           {  model.setParam('order', [ {col:'sales_bn', desc:true} ]);
+           } else
+           if (model.morder==2)
+           {  model.setParam('order', [ {col:'sales_bn'} ]);
+           } else
+           if (model.morder==0)
+           {  model.setParam('order', null);
+           }
+       });       
+   });
+
+   // Search
+   $('#tabsearch .model-list .model-search button.b-search').click(function(){
+         var s = $('.model-list .model-search input').val().trim();
+         var p = filterData.getData(true);
+         if (p.sic!=undefined) delete p.sic; //remove unused data
+         if (s!='' || p.filter!='')
+         {   if (s!='') p.search = '%'+s+'%';
+             model.load(p);
+         } else model.load();
+   });
+   
+   model.load();
+
+   $('#tabsearch  .model-list .model-search input').keyup(function(d){ 
+      if (d.keyCode==13)  $('.model-list .model-search button.b-search').trigger('click');
+   });
+  
+   // enable pager
+   pager = new modelPagination('.model-list .model-pager');
+  
+   let rows_lim = 7;
+
+   model.total(function(total, rows_lim){
+      pager.setTotal(total, rows_lim);
+      $('#tbedit').addClass('disabled');
+   })
+   
+   pager.change(function(n){
+      model.load(n);
+   });
+
+
+
 });
--- ajx-framework/pages/sales/screener.php
+++ ajx-framework/pages/sales/screener.php
@@ -5,6 +5,8 @@
     $f->labeled = false;
 ?>
 <div id="screener">
+    <div class="row">
+        <div class="col-lg-6">
  <?php
     $db = $this->cfg->db;
     $qr = $db->query('select * from sales_theams order by id');   
@@ -12,23 +14,60 @@
     while ($r = $db->fetchSingle($qr))
     {   
  ?>
-    <form class="form-inline w-themes row" data-id="<?=$r->id?>">
+    <form class="form-inline w-h-controls w-themes row" data-id="<?=$r->id?>">
         <div class="form-group">
             <label><?=$r->theam?></label>            
         </div>
         <div class="form-group w-slider">
-            <b style="padding-right: 10px;">-10</b>
+            <b class="w-lbl-left">-10</b>
             <input id="theme<?=($i++)?>" type="text" class="span2 bs-range" value="" data-control-type="range" data-slider-min="-10" data-slider-max="10" data-slider-step="1" data-slider-value="[-10,10]">
-            <b style="padding-left: 10px;">10</b>
+            <b class="w-lbl-right">10</b>
         </div>        
         <div class="form-group">
-            <input type="number" class="form-control" data-control-type="basic"  placeholder="Weight">
+            <input type="number" class="form-control w-short" data-control-type="basic"  placeholder="Weight">
         </div>        
     </form>
 
   <?php
     }
-  ?>
-   <div class="row" id="ranking"></div>
 
+    $item_list = ['', 'market_cap','sales_growth','EBITDA_growth','ROIC','ROE',
+    'price_to_earnings','ev_to_ebitda', 'yield', 'price_to_book', 'reinvestment',
+    'research_and_development', 'net_debt_to_EBITDA', 'CAPE', 'sustain_ex'];
+
+    $selector = '<select class="form-control">';
+    foreach($item_list as $v) $selector.='<option id="'.$v.'">'.T($v).'</option>';
+    $selector .= '</select>';
+?>  
+        </div>
+        <div class="col-lg-6">
+
+<?php
+    for ($i=1; $i<=5; $i++)
+    {
+?>
+    <form class="form-inline w-h-controls row">
+    <div class="form-group">
+        <?=$selector?>          
+    </div>
+    <div class="form-group w-slider">
+        <b class="w-lbl-left">-10</b>
+        <input id="range<?=($i)?>" type="text" class="span2 bs-range" value="" data-control-type="range" data-slider-min="-10" data-slider-max="10" data-slider-step="1" data-slider-value="[-10,10]">
+        <b class="w-lbl-right">10</b>
+    </div>        
+    </form>
+<?php
+    }
+?>
+        </div>
+    </div>
+
+    <div class="model-list" data-model="/pages/sales/Model/companies">
+        <table class="table table-striped selectable">
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+        <div class="model-pager"></div>
+    </div>
+    
 </div>
