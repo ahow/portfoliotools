1b3efcc8638ab8b0aff54b9c302f7f79085e20ed
Fedotov Vitaliy <kursruk@yandex.ru>
2019-12-06 15:27:43 +0800

    Move chart to the page bottom

--- ajx-framework/pages/sales/index.php
+++ ajx-framework/pages/sales/index.php
@@ -2,120 +2,118 @@
   <img class="card-img-top img-fluid w-logo"; style='width: 100%'; src="/images/volcano.jpg" alt="">
 </div>
 
-  <div class="row"> 
-    <div class="col-lg-12">
-      <h3>Themes comparison</h3>
-      <?php
-          require_once(SYS_PATH.'pages/sales/sales.php');          
-          $p = new sales($this->cfg, 'sales/themes-comparison', ['sales','themes-comparison']);
-          $p->smallHeader = true;
-          $p->display();
-      ?>
-    </div>  
+<div class="row">
+
+  <!-- /.col-lg-3 -->
+  <div class="col-lg-12">
+
+    <div class="card-body">
+      <h3 class="card-title">Investment theme analysis</h3>
+      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
+    </div>
   </div>
+        <!-- /.card -->
 
-  <div class="row">
-
-        <!-- /.col-lg-3 -->
-
-        <div class="col-lg-12">
-
-            <div class="card-body">
-              <h3 class="card-title">Investment theme analysis</h3>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
-            </div>
+  <div class="col-lg-12">
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            Thematic investing
           </div>
-          <!-- /.card -->
-
-    <div class="col-lg-12">
-          <div class="card card-outline-secondary my-4">
-            <div class="card-header">
-              Thematic investing
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-                  <div class="col-sm-6">
-                  <div>
-                    <iframe width=100% height="300" frameborder="0" scrolling="no" src="//plot.ly/~andywhoward/9.embed"></iframe>
-                  </div>
+          <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+                <div class="col-sm-6">
+                <div>
+                  <iframe width=100% height="300" frameborder="0" scrolling="no" src="//plot.ly/~andywhoward/9.embed"></iframe>
                 </div>
               </div>
-
-
-
-
-
-
             </div>
+
+
+
+
+
+
           </div>
+        </div>
 
-          <div class="card card-outline-secondary my-4">
-            <div class="card-header">
-              Performance drivers
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-              </div>
-            </div>
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            Performance drivers
           </div>
-
-          <div class="card card-outline-secondary my-4">
-            <div class="card-header">
-              Portfolio exposures
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card card-outline-secondary my-4">
-            <div class="card-header">
-              Theme tools
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card card-outline-secondary my-4">
-            <div class="card-header">
-              To do
-            </div>
-            <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                  </div>
-                  <div class="col-sm-6">
-                    <img style='width: 50%'; src="/images/logo.png">
-                  </div>
-              </div>
+          <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
             </div>
           </div>
         </div>
-        </div>
-      </div>
 
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            Portfolio exposures
+          </div>
+          <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            Theme tools
+          </div>
+          <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card card-outline-secondary my-4">
+          <div class="card-header">
+            To do
+          </div>
+          <div class="card-body">
+            <div class="row">
+                <div class="col-sm-6">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                </div>
+                <div class="col-sm-6">
+                  <img style='width: 50%'; src="/images/logo.png">
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+</div>  
+  
+    
+<div class="row"> 
+  <div class="col-lg-12">
+    <h3>Themes comparison</h3>
+    <?php
+        require_once(SYS_PATH.'pages/sales/sales.php');          
+        $p = new sales($this->cfg, 'sales/themes-comparison', ['sales','themes-comparison']);
+        $p->smallHeader = true;
+        $p->display();
+    ?>
+  </div>  
+</div>
